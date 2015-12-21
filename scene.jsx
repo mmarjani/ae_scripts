0d0cd90ea0c34bc3024dec82cb72fbf560c72c5c
primal100 <paul>
2015-12-21 10:19:01 +0000

    Second commit

--- scene.jsx
+++ scene.jsx
@@ -7,27 +7,31 @@ function doMovements(character){
     };
 
 function importCharacter(character){
-            character.folder = importComp(character.project_path, false);
-            var folder_items = character.folder.items; 
-            for ( var i = 1; i <  folder_items.length; i++ ) { 
-                if (folder_items[i] instanceof CompItem){ 
-                   character.comp =  folder_items[i];
-                   }
-            }
+        var audio_path = character.audio_path || false;        
+        var movements = character.movements || [];
+        character.folder = importComp(character.project_path, false);
+        var folder_items = character.folder.items; 
+        for ( var i = 1; i <  folder_items.length; i++ ) { 
+            if (folder_items[i] instanceof CompItem){ 
+               character.comp =  folder_items[i];
+               }
+        }
+        if (audio_path){
             var audio_layer = importAudio(character.audio_path);
             character.comp.layers.add(audio_layer);
             character.audio_layer = audio_layer.name;
-            setDuration(character.comp, duration);
-            if (character.sitting){
-                sittingDown(character.comp, 0);
-                }
-            for ( var i = 0; i <  character.movements.length; i++ ){
-                var movement = character.movements[i][0];
-                var args = character.movements[i][1];
-                args.unshift(character.comp);
-                movement.apply(this, args);
-                }
-            return character.comp;
+         }
+        setDuration(character.comp, duration);
+        if (character.sitting){
+            sittingDown(character.comp, 0);
+            }
+        for ( var i = 0; i <  movements.length; i++ ){
+            var movement = movements[i][0];
+            var args = character.movements[i][1];
+            args.unshift(character.comp);
+            movement.apply(this, args);
+            }
+        return character.comp;
 };
 
 function convertAudioWizard(characters){
@@ -43,9 +47,14 @@ function convertAudioWizard(characters){
 function initial_setup(){
     var project = getProject();
     var comp = newComp(scene_name, duration);
-    var backgroundComp = importImage(backgroundPath, false);
-    comp.layers.add(backgroundComp);
-    setDuration(backgroundComp, duration);
+    var backgroundComps = [];
+    for ( var i=0;  i < backgroundPaths.length; i++ ){
+        var backgroundPath = backgroundPaths[i];
+        var backgroundComp = importImage(backgroundPath, false);
+        backgroundComps.push(backgroundComp);
+        comp.layers.add(backgroundComp);
+        setDuration(backgroundComp, duration);
+    }
     for ( var i = 0; i <  characters.length; i++ ) { 
         comp.layers.add(importCharacter(characters[i])); 
      }
